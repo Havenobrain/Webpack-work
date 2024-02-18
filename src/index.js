94c91d7ec93a242782542c26a2ce6c6478398707
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-02-18 16:57:55 +0500

    f

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const ul = document.querySelector("ul");
+
+fetch("http://localhost:3000/users")
+    .then((x) => x.json())
+    .then((data) => {
+        for (let i = 0; i < data.length; i += 1) {
+            const user = data[i];
+            const listItem = `
+                <li>
+                    <p>Id = ${user.id}</p>
+                    <p>Name = ${user.name}</p>
+                    <p>Age = ${user.age}</p>
+                </li>
+            `;
+            ul.innerHTML += listItem;
+        }
+    });
+
